e9a7cd51ee5f8bf7f662b1590b2eabeef39cf241
Kurvesh Patel <patelkurvesh8866@gmail.com>
2024-09-10 09:07:00 +0000

    function import added

--- srv/service.cds
+++ srv/service.cds
@@ -1,11 +1,12 @@
 namespace zHRmgmt__oData.srv.service;
 
-using { zHRmgmt__oData.db.schema as db } from '../db/schema';
+using {zHRmgmt__oData.db.schema as db} from '../db/schema';
 
-service zHRmgmt__oData @(path : '/odata') {
+service zHRmgmt__oData @(path: '/odata') {
 
     entity EMPLOYEE as projection on db.EMPLOYEE;
-    entity Module as projection on db.MODULE;
-    entity CV as projection on db.CV;
+    entity Module   as projection on db.MODULE;
+    entity CV       as projection on db.CV;
+    function readModule(MODULE_TYPE : String) returns {};
 
 }
--- srv/service.js
+++ srv/service.js
@@ -1,7 +1,7 @@
 const cds = require("@sap/cds");
 
 module.exports = (srv => {
-    let { EMPLOYEE, MODULE, FILE } = srv.entities;
+    let {EMPLOYEE, MODULE, FILE} = srv.entities;
 
     srv.before("CREATE", EMPLOYEE, async (req) => {
         let db = await cds.connect.to('db');
@@ -17,12 +17,32 @@ module.exports = (srv => {
     });
 
     srv.before("DELETE", EMPLOYEE, async (req) => {
-        let { EMP_ID } = req.data;
+        let {EMP_ID} = req.data;
         let oEmployeeObj = {
             "sMessage": "Employee deleted successfully",
             "sDeepMessage": `Emplpoyee ${EMP_ID} deleted successfully`
         };
-        let { res } = req.http
+        let {res} = req.http
         res.send(oEmployeeObj)
+    });
+
+    srv.on("readModule", async (req) => {
+        let db = await cds.connect.to('db');
+        let tx = db.tx(req);
+        try {
+            var sQuery = `SELECT MODULE_ID,MODULE_NAME,MODULE_CODE FROM ${MODULE} WHERE MODULE_TYPE=${
+                req.data.MODULE_TYPE
+            }`;
+            var aModule = await tx.run(sQuery);
+            var oModuleObj = {
+                "Developed By": "Kurvesh Patel",
+                "status": 200,
+                "results": aModule
+            };
+            const {req} = req.http;
+            res.send(oModuleObj);
+        } catch (error) {
+            req.reject(404, error);
+        }
     });
 })
